0575448d939027dfcff2fc962a136fd2475bbd06
nandagoud <56551552+nandagoud@users.noreply.github.com>
2019-10-15 21:47:25 +0530

    NG Tech

---
+++ ngtech/src/main/java/com/ngtech/app/App.java
@@ -0,0 +1,13 @@
+package com.ngtech.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!...Created by Nandu .@_@." );
+    }
+}
---
+++ ngtech/src/main/java/com/ngtech/app/main.java
@@ -0,0 +1,11 @@
+package com.ngtech.app;
+
+public class main {
+
+
+        public static void main( String[] args )
+        {
+            System.out.println( "Hello World!...Created by Nandu .@_@." );
+        }
+
+}
